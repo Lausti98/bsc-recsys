76f1fb6af91f045e4cba81e0b6c45e5d6df0effd
Laust Skov Hansen <laust@MacBook-Pro-tilhrende-Laust.local>
2023-03-22 13:05:24 +0100

    Update normalized_discounted_cumulative_gain.py

--- src/metrics/normalized_discounted_cumulative_gain.py
+++ src/metrics/normalized_discounted_cumulative_gain.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 def cumulative_discounted_gain(arr):
-  nom = 2**arr-1
+  nom = 2.0**arr-1.0
   denom = np.log2(np.arange(len(arr)) + 2)
   return np.sum(nom / denom)
 
@@ -13,10 +13,11 @@ def normalized_cumulative_discounted_gain(pred, true):
   ncdg = dcg / idcg
   return ncdg
 
-res_arr = np.array([2, 3, 5])
-true_arr1 = np.array([2, 3, 5])
-true_arr2 = np.array([3, 5, 2])
-print(cumulative_discounted_gain(res_arr))
-print(normalized_cumulative_discounted_gain(res_arr, true_arr1))
-print(normalized_cumulative_discounted_gain(res_arr, true_arr2))
+if __name__ == '__main__':
+  res_arr = np.array([2, 3, 5])
+  true_arr1 = np.array([2, 3, 5])
+  true_arr2 = np.array([3, 5, 2])
+  print(cumulative_discounted_gain(res_arr))
+  print(normalized_cumulative_discounted_gain(res_arr, true_arr1))
+  print(normalized_cumulative_discounted_gain(res_arr, true_arr2))
 
