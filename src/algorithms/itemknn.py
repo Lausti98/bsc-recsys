f56d703041e7aed2ae867a95e40439a6d15a5fd6
Laust Skov Hansen <laust@MacBook-Pro-tilhrende-Laust.local>
2023-03-22 13:02:50 +0100

    Update itemknn to use adjusted cosine similarity and return predicted rating

--- src/algorithms/itemknn.py
+++ src/algorithms/itemknn.py
@@ -11,26 +11,48 @@ class ItemKNN:
     self.model = NearestNeighbors(n_neighbors=K, metric='cosine')
 
   def fit(self, X):
-    #TODO: Do checks on the input DF/Interactionmatrix
+    # Find mean user rating 
+    sums = X.sum(axis=0)
+    non_zeros = np.count_nonzero(X.toarray(), axis=0)
+    means = sums.flatten() / (non_zeros+(non_zeros==0)) # Account for possible divide by 0
 
-    self.data = X
-    #self.model.fit(X)
+    # Adjust ratings by means
+    X_transpose = X.transpose() 
+    nz = X_transpose.nonzero()
+    X_transpose = X_transpose.astype('float64')
+    X_transpose[nz] -= means[0, nz[0]]
+
+    # Assign mean adjusted data to self
+    self.data = X_transpose.transpose()
+    self.user_offset = means[0,:]
   
   def get_neighbors(self, X, verbose=False):
     """Get k closest neighbors by their cosine similarity"""
+
     # Find similarities
     similarities = cosine_similarity(X, self.data)
     index_arr = np.argsort(similarities, axis=1).flatten()[::-1] #reverse sorted array for descending order
 
+    # Get K neighbors from similarities
     neighbors = index_arr[:self.K]
-    distances = 1 - np.take(similarities, neighbors) # subtract 1 to make distance 0 optimal
+    distances = np.take(similarities, neighbors)
     
     if verbose:
       for i in range(len(distances)):
         print(f'Item {i+1}:  Distance - {distances[i]}, item ID - {neighbors[i]}')
+    
+
     return distances, neighbors
   
-  def predict(self, X):
-    #TODO: Implement actual predictions 
-    _, neighbors = self.get_neighbors(X)
-    return neighbors
+  def predict(self, X, user):
+
+    # Get the nearest neighbors
+    dist, neighbors = self.get_neighbors(X)
+
+    # Calculate prediction based on neighbors
+    rating = 0
+    for d, n in zip(dist, neighbors):
+      rating += self.data[n,user]*d
+    prediction = (rating/(dist.sum()+(dist.sum()==0.0))) + self.user_offset[0, user]
+
+    return prediction if prediction != None else 0.0
