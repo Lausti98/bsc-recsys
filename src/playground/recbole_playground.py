1972b2f92f1c2b8dfa0b4d4dd8cc40e8d64eb457
Laust Skov Hansen <laust@MacBook-Pro-tilhrende-Laust.local>
2023-03-28 20:12:57 +0200

    explorative use of daisyrec

--- src/playground/recbole_playground.py
+++ src/playground/recbole_playground.py
@@ -1,12 +1,15 @@
 #pylint: skip-file
-from recbole.model.general_recommender import ItemKNN
+from recbole.model.general_recommender import ItemKNN, Pop
+from recbole.data import Interaction
 from src.dataloader import load_dataset
 # from src.benchmarks.nearest_neighbor import prepare
+
 
 df = load_dataset.steam()
 # csr = prepare(df)
 
+interaction = Interaction(df.sort_values(['user_id', 'rating']))
 # print(csr)
 
-model = ItemKNN(config={'k': 10, 'USER_ID_FIELD': 'user_id', 'ITEM_ID_FIELD': 'item_id', 'NEG_PREFIX': ''}, dataset=df)
+model = Pop(dataset=interaction)
 print(model)
