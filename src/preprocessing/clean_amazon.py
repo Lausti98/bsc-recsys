f553c4cf47943d6f2582d623a548aef1641359d7
Laust Skov Hansen <laust@MacBook-Pro-tilhrende-Laust.local>
2023-04-05 15:13:04 +0200

    Update clean_amazon.py

--- src/preprocessing/clean_amazon.py
+++ src/preprocessing/clean_amazon.py
@@ -1,18 +1,23 @@
 import pandas as pd
 from sklearn import preprocessing
 
-fname = 'Prime_Pantry.json'
-fpath = f'data/raw/{fname}'
+fname = 'Software.json'
+fpath = f'../../data/raw/{fname}'
+meta_fpath = f'../../data/raw/meta_{fname}'
 
 # Load and proces data
 df = pd.read_json(fpath, lines=True)
+meta = pd.read_json(meta_fpath, lines=True)
 le = preprocessing.LabelEncoder()
 # use only verified reviews
 df = df[df['verified'] == True]
+
+# Merge title for each product
+df = df.merge(meta[['asin', 'title']], on='asin', how='inner')
+
 df['product_id'] = le.fit_transform(df['asin'])
 df['user_id'] = le.fit_transform(df['reviewerID'])
-print(df.dtypes)
 
 # Remove unneeded columns
-df = df[['user_id', 'product_id', 'overall']]
-df.to_csv(f'data/{fname.split(sep=".")[0].lower()}_rating_only.csv')
+df = df[['user_id', 'product_id', 'overall', 'title']]
+df.to_csv(f'../../data/{fname.split(sep=".")[0].lower()}_rating_only.csv')
