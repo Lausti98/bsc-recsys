010d141b469996e6a06592a516988c1a5b76546d
Laust Skov Hansen <laust@MacBook-Pro-tilhrende-Laust.local>
2023-05-31 16:27:34 +0200

    Add stemming of tokens

--- src/feature_extraction/vectorize.py
+++ src/feature_extraction/vectorize.py
@@ -8,17 +8,23 @@ import gensim.downloader
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 from string import punctuation
 
 nltk.download('punkt')
 nltk.download('stopwords')
 stopwords = stopwords.words('english')
 punct = list(punctuation)
+stemmer = PorterStemmer()
 
 def tokenize(text):
   text = text.replace("'", "")
   token_arr = word_tokenize(text.lower())
-  return remove_stop_and_punct(token_arr)
+  stemmed = stem_tokens(token_arr)
+  return remove_stop_and_punct(stemmed)
+
+def stem_tokens(token_arr):
+  return [stemmer.stem(token) for token in token_arr]
 
 def remove_stop_and_punct(token_arr):
   return [token for token in token_arr if token not in stopwords and token not in punct]
@@ -43,10 +49,17 @@ def get_pretrained_w2v(title_arr):
   title_arr = title_arr.fillna("empty")
   model = gensim.downloader.load('glove-twitter-100')
   # Clean title_tokens
+  def_vect = np.zeros(model.vector_size)
   title_arr = title_arr.fillna("empty")
   clean_tokens = [tokenize(i) for i in title_arr]
+  title_mat = np.zeros((len(title_arr), model.vector_size))
+  for idx, title in enumerate(clean_tokens):
+    # title_vec = def_vect
+    for token in title:
+      title_mat[idx] += model[token] if token in model else def_vect
+      
 
-  return model, clean_tokens
+  return model, title_mat
 
 if __name__ == '__main__':
   df = pd.read_csv('../../data/steam-200k.csv')
