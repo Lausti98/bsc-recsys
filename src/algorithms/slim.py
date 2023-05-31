89a0663b289f989dc0719b9fe15c8c05cf997cf6
Laust Skov Hansen <laust@MacBook-Pro-tilhrende-Laust.local>
2023-05-31 16:23:57 +0200

    Update slim.py

--- src/algorithms/slim.py
+++ src/algorithms/slim.py
@@ -1,22 +1,37 @@
+# pylint: skip-file
 import numpy as np
 from daisy.model.SLiMRecommender import SLiM
 
+from sklearn.preprocessing import MinMaxScaler
+
+import warnings
+warnings.simplefilter('ignore')
 
 class SLiMRec:
-  def __init__(self, config, elastic=0.1, alpha=1.0):
+  def __init__(self, config, elastic=0.1, alpha=1.0, scale=False):
     config['elastic'] = elastic
     config['alpha'] = alpha
     self.config = config
     self.elastic = elastic
     self.alpha = alpha
     self.model = SLiM(config)
+    self.scale = scale
 
   def fit(self, X):
     ## May need to do preprocessing steps on train data
-    self.model.fit(X)
+    self.X = X.copy()
+    if self.scale:
+      scaler = MinMaxScaler()
+      self.X['rating'] = scaler.fit_transform(
+        self.X['rating'].values.reshape(-1,1))
 
-  def predict():
-    pass
+    self.model.fit(self.X)
+
+  def predict(self, user, item=None):
+    if item: 
+      return self.model.predict(user, item)
+    else:
+      return self.model.A_tilde[user, :].A.squeeze()
 
   def rank(self, test_df):
     t_users = test_df['user'].unique()
@@ -25,10 +40,13 @@ class SLiMRec:
       for user in t_users:
         ranks.append(self.model.full_rank(user))
     else:
-      ranks.append(self.model.full_rank(user))
+      ranks.append(self.model.full_rank(test_df['user']))
     
     return np.array(ranks)
   
+  def full_rank(self, user):
+    return self.model.full_rank(user)
+
   def set_params(self, alpha=None, elastic=None):
     if alpha:
       self.alpha = alpha
